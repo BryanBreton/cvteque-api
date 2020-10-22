b280c1985e1973970532594669b225452f173170
Breton <bryan.breton@epsi.fr>
2020-10-22 16:56:57 +0200

    feat(offre): first route

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express')
 const app = express()
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'CVTeque',
+  password: 'admin',
+  port: 5432,
+})
+
+
+app.get('/offres', (request, response) => {
+    pool.query('SELECT * FROM etudiant ORDER BY id ASC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+})
 
 app.listen(3000, () => {
     console.log("j'ecoute au port 3000 frr");
